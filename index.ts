87f2df7ca3e00e356b491d874a8031840ca1ab9f
unknown <usuario@PUERTA18.CORP.AR>
2022-06-22 14:11:26 -0300

    Model

--- index.ts
+++ index.ts
@@ -1,17 +1,41 @@
+import { Socie } from "./model/socie.model";
 // 1. Select the div element using the id property
 const app = document.getElementById("app");
-// 2. Create a new <p></p> element programmatically
-const p = document.createElement("p");
-// 3. Add the text content
-p.textContent = "Bienvenido al pais de las maravillas";
-// 4. Append the p element to the div element
-app?.appendChild(p);
+//Defino a un array de tipo Socie, lo inicio vacio//
+let socies:Array<Socie>=[]
+//Frech que devuelve los datos
 
 fetch("http://localhost:5000/socies/")
 .then (resp =>{
     resp.json()
     .then(data=>{
-        console.log(data.mensaje);
-        app?.appendChild(p);
+        socies=data.socies      ;
     })
 });
+
+//For loop de cada socie dentro del Array
+socies.forEach(socies => {
+    let p_nombre = document.createElement("p");
+    p_nombre.textContent = socies.nombre;
+    app?.appendChild(p_nombre)
+    
+});
+
+//for loop de cada socie dentro del array
+socies.forEach(socie=>{
+    let tr=document.createElement("tr");
+    //Nombre
+    let td_nombre=document.createElement("td");td_nombre.textContent=socie.nombre;
+    //Apellido
+    let td_apellido=document.createElement("td");td_apellido.textContent=socie.apellido;
+    //Nickname
+    let td_nick=document.createElement("td");td_nick.textContent=socie.nick
+    //Edad
+    let td_edad=document.createElement("td");td_edad.textContent=socie.edad.toString(10);
+    
+        tr?.appendChild(td_nombre);
+        tr?.appendChild(td_apellido);
+        tr?.appendChild(td_nick);
+        tr?.appendChild(td_edad);
+        app?.appendChild(tr);
+    })
